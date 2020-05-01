f3e1401571bd6c942d80d76b4346a5447d3fa461
Phil Barber <phil@lantum.com>
2020-05-01 11:31:57 +0100

    Add linting

--- contact-form-api/handler.js
+++ contact-form-api/handler.js
@@ -1,37 +1,38 @@
-const aws = require('aws-sdk')
-const ses = new aws.SES()
-const myEmail = process.env.EMAIL
-const myDomain = process.env.DOMAIN
+const aws = require("aws-sdk");
+const ses = new aws.SES();
+const myEmail = process.env.EMAIL;
+const myDomain = process.env.DOMAIN;
 
 const RESPONSE_HEADERS = {
-  'Access-Control-Allow-Origin': myDomain,
-  'Access-Control-Allow-Headers': 'x-requested-with',
-  'Access-Control-Allow-Credentials': true
-}
+  "Access-Control-Allow-Origin": myDomain,
+  "Access-Control-Allow-Headers": "x-requested-with",
+  "Access-Control-Allow-Credentials": true,
+};
 
-function generateResponse (code, payload) {
+function generateResponse(code, payload) {
   return {
     statusCode: code,
     headers: RESPONSE_HEADERS,
-    body: JSON.stringify(payload)
-  }
+    body: JSON.stringify(payload),
+  };
 }
 
-function generateError (code, err) {
-  console.log(err)
+function generateError(code, err) {
+  console.log(err);
   return {
     statusCode: code,
     headers: RESPONSE_HEADERS,
-    body: JSON.stringify(err.message)
-  }
+    body: JSON.stringify(err.message),
+  };
 }
 
-function generateEmailParams (body) {
-  const { email, name, content } = JSON.parse(body)
-  console.log(email, name, content)
+function generateEmailParams(body) {
+  const { email, name, content } = JSON.parse(body);
+  console.log(email, name, content);
   if (!(email && name && content)) {
     throw new Error(
-      'Missing parameters! Make sure to add parameters \'email\', \'name\', \'content\'.')
+      "Missing parameters! Make sure to add parameters 'email', 'name', 'content'."
+    );
   }
 
   return {
@@ -41,25 +42,24 @@ function generateEmailParams (body) {
     Message: {
       Body: {
         Text: {
-          Charset: 'UTF-8',
-          Data: `Message sent from email ${email} by ${name} \nContent: ${content}`
-        }
+          Charset: "UTF-8",
+          Data: `Message sent from email ${email} by ${name} \nContent: ${content}`,
+        },
       },
       Subject: {
-        Charset: 'UTF-8',
-        Data: `You received a message from ${myDomain}!`
-      }
-    }
-  }
+        Charset: "UTF-8",
+        Data: `You received a message from ${myDomain}!`,
+      },
+    },
+  };
 }
 
 module.exports.send = async (event) => {
   try {
-    const emailParams = generateEmailParams(event.body)
-    const data = await ses.sendEmail(emailParams).promise()
-    return generateResponse(200, data)
+    const emailParams = generateEmailParams(event.body);
+    const data = await ses.sendEmail(emailParams).promise();
+    return generateResponse(200, data);
   } catch (err) {
-    return generateError(500, err)
+    return generateError(500, err);
   }
-}
-
+};
